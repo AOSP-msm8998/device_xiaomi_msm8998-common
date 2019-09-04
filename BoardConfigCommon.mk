e7deb2935d713caf8fff823e2348cd994a3ac797
Volodymyr Zhdanov <wight554@gmail.com>
2019-09-04 17:44:21 +0300

    [DNM] msm8998-common: Permisive for now
    Change-Id: Id6183a5f50f28e3e81cbf7562e5d9cefc655a414

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,6 +48,7 @@ BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8998
