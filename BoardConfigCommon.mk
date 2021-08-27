d0e3f54f1d5f46b47c8923da4b32ce806a3c7bc7
ArianK16a <ArianK16a@users.noreply.github.com>
2021-08-27 16:27:23 +0300

    [DNM] msm8998-common: Disable sepolicy and Add minimal sepolicy
    Change-Id: Ia74de4410962e61f6e3b15c6320c48946f0c16f1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,10 +126,11 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2019-09-01
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
-PRODUCT_PRIVATE_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/private
-PRODUCT_PUBLIC_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/public
+# include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+# BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
+# PRODUCT_PRIVATE_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/private
+# PRODUCT_PUBLIC_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/public
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy-minimal
 
 # Treble
 BOARD_VNDK_VERSION := current
