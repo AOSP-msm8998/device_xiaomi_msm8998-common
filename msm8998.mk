9974ef49ca79a9c3fc6a9057ba408b57ad6bf08c
Krzysztof Kosiński <krzysio@google.com>
2022-11-11 05:41:10 +0000

    msm8998-common: Add prebuilt protobuf 3.9.1 to vendor partition.
    Needed to work around a Qualcomm HAL prebuilt.
    
    Bug: 203713560
    Bug: 258322017
    Bug: 254854421
    Test: Forrest
    Change-Id: I4eebc60f10fbea6a66127ed3e08441db32536f78

--- msm8998.mk
+++ msm8998.mk
@@ -357,8 +357,7 @@ PRODUCT_PACKAGES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat
 
 # QCOM
 PRODUCT_COPY_FILES += \
